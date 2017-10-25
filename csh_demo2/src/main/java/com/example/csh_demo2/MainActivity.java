c7a84b7e2d74f239cad1577919199140ec87f0a2
androidQAQ <1023078708@qq.com>
2017-10-25 20:13:12 +0800

    Xrecycler实现下拉加载

---
+++ csh_demo2/src/main/java/com/example/csh_demo2/MainActivity.java
@@ -0,0 +1,72 @@
+package com.example.csh_demo2;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+
+import com.bwei.okhttp3ps.utils.GsonObjectCallback;
+import com.bwei.okhttp3ps.utils.OkHttp3Utils;
+import com.jcodecraeer.xrecyclerview.XRecyclerView;
+import com.squareup.picasso.Picasso;
+
+import java.io.IOException;
+import java.util.List;
+
+import okhttp3.Call;
+
+public class MainActivity extends AppCompatActivity {
+
+    private XRecyclerView Xrecycler;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //找控件
+        Xrecycler= (XRecyclerView) findViewById(R.id.Xrcycler);
+        Xrecycler.setLayoutManager(new LinearLayoutManager(this));
+        Xrecycler.addItemDecoration(new MyDecoration(this,MyDecoration.VERTICAL_LIST));
+        Xrecycler.setLoadingListener(new XRecyclerView.LoadingListener() {
+            @Override
+            public void onRefresh() {
+                //请求数据
+                getData();
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        Xrecycler.refreshComplete();
+                    }
+                },2000);
+            }
+
+            @Override
+            public void onLoadMore() {
+
+            }
+        });
+      //请求数据
+        getData();
+    }
+
+    private void getData() {
+        //设置适配器
+        String url="http://news-at.zhihu.com/api/4/news/latest";
+
+        OkHttp3Utils.doGet(url, new GsonObjectCallback<Bean>() {
+            @Override
+            public void onUi(Bean bean) {
+                List<Bean.StoriesBean> stories = bean.getStories();
+
+                MyAdapter adapter = new MyAdapter(MainActivity.this,stories);
+                Xrecycler.setAdapter(adapter);
+            }
+            @Override
+            public void onFailed(Call call, IOException e) {
+            }
+        });
+
+
+    }
+
+
+}
---
+++ csh_demo2/src/main/java/com/example/csh_demo2/MyAdapter.java
@@ -0,0 +1,60 @@
+package com.example.csh_demo2;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bwei.okhttp3ps.utils.GsonObjectCallback;
+import com.bwei.okhttp3ps.utils.OkHttp3Utils;
+import com.jcodecraeer.xrecyclerview.XRecyclerView;
+import com.squareup.picasso.Picasso;
+
+import java.io.IOException;
+import java.util.List;
+
+import okhttp3.Call;
+
+/**
+ * Created by 曹少航 on 2017/10/25.
+ */
+
+public class MyAdapter extends XRecyclerView.Adapter<MyAdapter.MyViewHolder> {
+    private Context context;
+    private List<Bean.StoriesBean> stories;
+    private  ImageView image1;
+    private  TextView tv1;
+
+    public MyAdapter(Context context, List<Bean.StoriesBean> stories) {
+        this.context = context;
+        this.stories = stories;
+    }
+
+    @Override
+    public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view= LayoutInflater.from(context).inflate(R.layout.item,null);
+        MyViewHolder holder=new MyViewHolder(view);
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(MyViewHolder holder, final int position) {
+        Picasso.with(context).load(stories.get(position).getImages().get(0)).into(image1);
+        tv1.setText(stories.get(position).getTitle());
+    }
+    @Override
+    public int getItemCount() {
+        return stories.size();
+    }
+
+    class MyViewHolder extends XRecyclerView.ViewHolder{
+        public MyViewHolder(View itemView) {
+            super(itemView);
+            image1 = itemView.findViewById(R.id.image1);
+            tv1 = itemView.findViewById(R.id.tv1);
+
+        }
+    }
+}
