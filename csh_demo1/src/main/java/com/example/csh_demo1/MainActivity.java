da63a460b31af8737507a7b9af2bd5d0291fe91e
androidQAQ <1023078708@qq.com>
2017-11-05 19:30:50 +0800

    底部导航+Tablay+Retrofit+RxJava

--- csh_demo1/src/main/java/com/example/csh_demo1/MainActivity.java
+++ csh_demo1/src/main/java/com/example/csh_demo1/MainActivity.java
@@ -4,6 +4,10 @@ import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.csh_demo1.fragment.Fragment1;
+import com.example.csh_demo1.fragment.Fragment2;
+import com.example.csh_demo1.fragment.Fragment3;
+import com.example.csh_demo1.fragment.Fragment4;
 import com.hjm.bottomtabbar.BottomTabBar;
 
 public class MainActivity extends AppCompatActivity {
@@ -20,10 +24,10 @@ public class MainActivity extends AppCompatActivity {
                 .setFontSize(12)
                 .setTabPadding(4, 6, 10)
                 .setChangeColor(Color.RED, Color.BLACK)
-                .addTabItem("第一页", R.mipmap.ic_nav_cart_press,R.mipmap.ic_nav_cart,  Fragment1.class)
-                .addTabItem("第二页",  R.mipmap.ic_nav_home_press,R.mipmap.ic_nav_home, Fragment2.class)
-                .addTabItem("第三页",  R.mipmap.ic_nav_user_press,R.mipmap.ic_nav_user, Fragment3.class)
-                .addTabItem("第四页", R.mipmap.ic_nav_class_press, R.mipmap.ic_nav_class, Fragment4.class) ;
+                .addTabItem("首页", R.mipmap.ic_nav_cart_press,R.mipmap.ic_nav_cart,  Fragment1.class)
+                .addTabItem("自选",  R.mipmap.ic_nav_home_press,R.mipmap.ic_nav_home, Fragment2.class)
+                .addTabItem("行情",  R.mipmap.ic_nav_user_press,R.mipmap.ic_nav_user, Fragment3.class)
+                .addTabItem("资讯", R.mipmap.ic_nav_class_press, R.mipmap.ic_nav_class, Fragment4.class) ;
 
     }
 
